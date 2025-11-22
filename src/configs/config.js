f4cc5671c250c0a64164bf194891678242f1ca8e
Tamizmani05 <tamizhmani13270@gmail.com>
2025-11-22 09:16:44 +0530

    feat: Env setup

---
+++ src/configs/config.js
@@ -0,0 +1,10 @@
+const dotenvFlow = require('dotenv-flow');
+
+dotenvFlow.config();
+
+module.exports = {
+  ENV: process.env.ENV,
+  PORT: process.env.PORT,
+  SERVER_URL: process.env.SERVER_URL,
+  DATABASE_URL: process.env.DATABASE_URL,
+};
