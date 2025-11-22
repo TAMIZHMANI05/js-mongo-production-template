35c2daba101234388d1e1aa8b3a23fd6a3ca5869
Tamizmani05 <tamizhmani13270@gmail.com>
2025-11-22 10:10:18 +0530

    feat: Helmet Setup

--- src/app.js
+++ src/app.js
@@ -4,16 +4,23 @@ const router = require("./route/apiRouter");
 const globalErrorHandler = require("./middlewares/globalErrorHandler");
 const responseMessage = require("./constants/responseMessage");
 const httpError = require("./utils/httpError");
+const helmet = require("helmet");
 
 const app = express();
 
+// Json Middleware
 app.use(express.json());
 
+// Security Middleware
+app.use(helmet());
+
+// Static Files Middleware
 app.use(express.static(path.join(__dirname, "../", "public")));
 
+// API Routes
 app.use("/api/v1", router);
 
-
+// 404 Middleware
 app.use((req, _res, next) => {
   try {
     throw new Error(responseMessage.NOTFOUND("Route"));
@@ -22,6 +29,7 @@ app.use((req, _res, next) => {
   }
 });
 
+// Global Error Handler Middleware
 app.use(globalErrorHandler);
 
 module.exports = app;
